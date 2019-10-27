557ade781ea5a9661e96576ff90f69dc1c86b76c
Arturo <arturo.rivera@metronom.com>
2019-10-27 13:46:31 +0100

    Fixed error of mongoDB connection

--- config/mongodbconfig.go
+++ config/mongodbconfig.go
@@ -12,24 +12,29 @@ type MongoClient struct {
 	Client        *mongo.Client
 }
 
-func NewMongoConnection() (*MongoClient, error) {
+func NewMongoConnection() *MongoClient {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatalf("Unable to connect to MongoDB: %s\n", err.Error())
-		return nil, err
+		return nil
 	}
-	log.Printf("New connection to MongoDB successful.")
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatalf("Unable to ping to MongoDB server: %s\n", err.Error())
+		return nil
+	}
+	log.Println("New connection to MongoDB successful.")
 	return &MongoClient{
 		clientOptions,
 		client,
-	}, nil
+	}
 }
 
 func (m *MongoClient) CloseMongoConnection() error {
 	err := m.Client.Disconnect(context.TODO())
 	if err != nil {
-		log.Fatalf("Unable to close connection to MongoDB: %s\n", err.Error())
+		log.Printf("Unable to close connection to MongoDB: %s\n", err.Error())
 		return err
 	}
 	log.Println("Disconnected from MongoDB successfully.")
--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"context"
 	"issue-tracker/config"
 	"log"
 )
 
 func main() {
-	mongoClient, err := config.NewMongoConnection()
-	if err != nil {
-		panic(err)
-	}
+	mongoClient := config.NewMongoConnection()
 
-	err = mongoClient.Client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Ping to mongoDB successful")
-
-	err = mongoClient.CloseMongoConnection()
+	err := mongoClient.CloseMongoConnection()
 	if err != nil {
 		log.Fatal(err)
 	}
