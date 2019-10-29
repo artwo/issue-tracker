98e4964e05dce268e3a584a9ed2135bc4c993a20
Arturo <arturo.rivera@metronom.com>
2019-10-29 13:33:22 +0100

    Added Rest Controller

---
+++ controller/restcontroller.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/unrolled/render"
+	"issue-tracker/repo"
+	"issue-tracker/service"
+	"net/http"
+)
+
+type RestController struct {
+	*render.Render
+	TicketRepo    repo.TicketRepository
+	TicketService service.TicketService
+}
+
+func newJsonRender() *render.Render {
+	return render.New(render.Options{
+		IndentJSON: true,
+	})
+}
+
+func NewRestController(ticketRepo repo.TicketRepository, ticketService service.TicketService) *RestController {
+	return &RestController{
+		newJsonRender(),
+		ticketRepo,
+		ticketService,
+	}
+}
+
+func (c *RestController) Routes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Get("/", c.GetAllTickets)
+	return router
+}
+
+func (c *RestController) GetAllTickets(w http.ResponseWriter, r *http.Request) {
+	tickets := c.TicketService.GetAllTickets()
+	_ = c.JSON(w, http.StatusOK, tickets)
+}
